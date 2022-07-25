27e70fc51c058e4c3e28de122830c4ca9d089b59
Ricardo Castillo <ricardoacz20@gmail.com>
2022-07-25 12:11:08 -0400

    charmander and squirtle are objects

--- pokemon-game-script.js
+++ pokemon-game-script.js
@@ -1,39 +1,39 @@
 //Life bar variables
-let enemyHp = 100
-let userHp = 100
+// let enemyHp = 100
+// let userHp = 100
 
 //Charmander Object Code
 let charmander = {}
-charmander.hp - 100
+charmander.hp = 100
 charmander.scratch = 40
 charmander.growl = 2
 charmander.speed = 2
 charmander.defense = 20
 
 charmander.useScratch = function(){
-    enemyHp = enemyHp + squirtleDefense - charmander.scratch
+    squirtle.hp = squirtle.hp + squirtle.defense - charmander.scratch
     
 
 
     messageBattle.innerText = 'Charmander used scratch!'
 
     //converting number to string and adding % sign to affect css life bar
-    document.querySelector('#enemy-life').style.width = `${String(enemyHp)}%`
+    document.querySelector('#enemy-life').style.width = `${String(squirtle.hp)}%`
 
     //Computer's turn
-    if(enemyHp > 0) {
+    if(squirtle.hp > 0) {
         pcMove()
     }
     
 
-    if(enemyHp <= 0 && userHp > 0) {
+    if(squirtle.hp <= 0 && charmander.hp > 0) {
         messageFinal.innerText = 'Charmander won!'
         document.querySelector('#squirtle').style.display = 'none'
         document.querySelector('.moves').style.display = 'none'
         document.querySelector('#play-again').style.display = 'block'
         document.querySelector('#enemy-life').style.display = 'none'
         messageBattle2.style.display = 'none'
-    } else if (enemyHp <= 0 && userHp <= 0){
+    } else if (squirtle.hp <= 0 && charmander.hp <= 0){
         messageFinal.innerText = 'Tie!'
         document.querySelector('#squirtle').style.display = 'none'
         document.querySelector('#charmander').style.display = 'none'
@@ -44,11 +44,78 @@ charmander.useScratch = function(){
     }
 }
 
+charmander.useGrowl = function(){
+    if(squirtle.tackle >= 10){
+        squirtle.tackle = squirtle.tackle - charmander.growl
+    } else {
+        squirtle.tackle = 10
+    }
+    
+    
+
+
+    messageBattle.innerText = "Charmander used growl! Squirtle's attack was reduced"
+
+    //Computer's turn
+    if(squirtle.hp > 0) {
+        pcMove()
+    }
+}
+
 //Squirtle
 let tackle = 40
 let tailWhip = 2
 let squirtleSpeed = 1
 let squirtleDefense = 20
+
+//Squirtle Object Code
+let squirtle = {}
+squirtle.hp = 100
+squirtle.tackle = 40
+squirtle.tailWhip = 2
+squirtle.speed = 1
+squirtle.defense = 20
+
+squirtle.useTackle = function() {
+    charmander.hp = charmander.hp + charmander.defense - squirtle.tackle
+    
+
+
+    messageBattle2.innerText = 'Squirtle used tackle!'
+
+    document.querySelector('#user-life').style.width = `${String(charmander.hp)}%`
+
+    if(charmander.hp <= 0 && squirtle.hp > 0) {
+        messageFinal.innerText = 'Squirtle won!'
+
+        document.querySelector('#charmander').style.display = 'none'
+        document.querySelector('.moves').style.display = 'none'
+        document.querySelector('#play-again').style.display = 'block'
+        document.querySelector('#user-life').style.display = 'none'
+        messageBattle.style.display = 'none'
+    } else if (squirtle.hp <= 0 && charmander.hp <= 0){
+        messageFinal.innerText = 'Tie!'
+        document.querySelector('#squirtle').style.display = 'none'
+        document.querySelector('#charmander').style.display = 'none'
+        document.querySelector('.moves').style.display = 'none'
+        document.querySelector('#play-again').style.display = 'block'
+        document.querySelector('#enemy-life').style.display = 'none'
+        document.querySelector('#user-life').style.display = 'none'
+    }
+}
+
+squirtle.useTailwhip = function(){
+    if(charmander.defense >= 0){
+        charmander.defense = charmander.defense - squirtle.tailWhip
+    } else {
+        charmander.defense = 0
+    }
+    
+    
+
+
+    messageBattle2.innerText = "Squirtle used Tail Whip! Charmander's defense was reduced"
+}
 
 
 
@@ -59,7 +126,7 @@ let messageFinal = document.querySelector('#final-message')
 
 //Click interations, charmander attacks and reset game btn
 document.querySelector('#scratch').addEventListener('click', charmander.useScratch)
-document.querySelector('#growl').addEventListener('click', useGrowl)
+document.querySelector('#growl').addEventListener('click', charmander.useGrowl)
 document.querySelector('#play-again').addEventListener('click', playAgain)
 
 //Charmander
@@ -106,24 +173,24 @@ let charmanderDefense = 20
 // }
 
 //Charmander's non attacking move
-function useGrowl(){
+// function useGrowl(){
     
-    if(tackle >= 10){
-        tackle = tackle - growl
-    } else {
-        tackle = 10
-    }
+//     if(tackle >= 10){
+//         tackle = tackle - growl
+//     } else {
+//         tackle = 10
+//     }
     
     
 
 
-    messageBattle.innerText = "Charmander used growl! Squirtle's attack was reduced"
+//     messageBattle.innerText = "Charmander used growl! Squirtle's attack was reduced"
 
-    //Computer's turn
-    if(enemyHp > 0) {
-        pcMove()
-    }
-}
+//     //Computer's turn
+//     if(enemyHp > 0) {
+//         pcMove()
+//     }
+// }
 
 //Random number (1 or 2)
 function chance() {
@@ -134,76 +201,76 @@ function chance() {
 function pcMove(){
     if (chance() === 1) {
         //function for Tackle attack
-        useTackle()
+        squirtle.useTackle()
     } else {
         //function for tail whip
-        useTailwhip()
+        squirtle.useTailwhip()
     }
 }
 
 //Squirtle's attack
-function useTackle(){
-    userHp = userHp + charmanderDefense - tackle
+// function useTackle(){
+//     userHp = userHp + charmanderDefense - tackle
     
 
 
-    messageBattle2.innerText = 'Squirtle used tackle!'
+//     messageBattle2.innerText = 'Squirtle used tackle!'
 
-    document.querySelector('#user-life').style.width = `${String(userHp)}%`
+//     document.querySelector('#user-life').style.width = `${String(userHp)}%`
 
-    if(userHp <= 0 && enemyHp > 0) {
-        messageFinal.innerText = 'Squirtle won!'
+//     if(userHp <= 0 && enemyHp > 0) {
+//         messageFinal.innerText = 'Squirtle won!'
 
-        document.querySelector('#charmander').style.display = 'none'
-        document.querySelector('.moves').style.display = 'none'
-        document.querySelector('#play-again').style.display = 'block'
-        document.querySelector('#user-life').style.display = 'none'
-        messageBattle.style.display = 'none'
-    } else if (enemyHp <= 0 && userHp <= 0){
-        messageFinal.innerText = 'Tie!'
-        document.querySelector('#squirtle').style.display = 'none'
-        document.querySelector('#charmander').style.display = 'none'
-        document.querySelector('.moves').style.display = 'none'
-        document.querySelector('#play-again').style.display = 'block'
-        document.querySelector('#enemy-life').style.display = 'none'
-        document.querySelector('#user-life').style.display = 'none'
-    }
-}
+//         document.querySelector('#charmander').style.display = 'none'
+//         document.querySelector('.moves').style.display = 'none'
+//         document.querySelector('#play-again').style.display = 'block'
+//         document.querySelector('#user-life').style.display = 'none'
+//         messageBattle.style.display = 'none'
+//     } else if (enemyHp <= 0 && userHp <= 0){
+//         messageFinal.innerText = 'Tie!'
+//         document.querySelector('#squirtle').style.display = 'none'
+//         document.querySelector('#charmander').style.display = 'none'
+//         document.querySelector('.moves').style.display = 'none'
+//         document.querySelector('#play-again').style.display = 'block'
+//         document.querySelector('#enemy-life').style.display = 'none'
+//         document.querySelector('#user-life').style.display = 'none'
+//     }
+// }
 
 //Squirtle's non attacking move
-function useTailwhip(){
-    if(charmanderDefense >= 0){
-        charmanderDefense = charmanderDefense - tailWhip
-    } else {
-        charmanderDefense = 0
-    }
+// function useTailwhip(){
+//     if(charmanderDefense >= 0){
+//         charmanderDefense = charmanderDefense - tailWhip
+//     } else {
+//         charmanderDefense = 0
+//     }
     
     
 
 
-    messageBattle2.innerText = "Squirtle used Tail Whip! Charmander's defense was reduced"
-}
+//     messageBattle2.innerText = "Squirtle used Tail Whip! Charmander's defense was reduced"
+// }
     
 
 //RESET GAME
 function playAgain() {
     
     
-    //resetting variables to their original value
-    enemyHp = 100
-    userHp = 100
+    
     
     //Squirtle
-    tackle = 40
-    tailWhip = 2
-    squirtleSpeed = 1
-    squirtleDefense = 20
+    squirtle.hp = 100
+    squirtle.tackle = 40
+    squirtle.tailWhip = 2
+    squirtle.speed = 1
+    squirtle.defense = 20
     
     //Charmander
-    scratch = 40
-    growl = 2
-    speedCharmander = 2
-    charmanderDefense = 20
+    charmander.hp = 100
+    charmander.scratch = 40
+    charmander.growl = 2
+    charmander.speed = 2
+    charmander.defense = 20
     
     //showing and hiding css parts
     messageBattle.innerText = ''
